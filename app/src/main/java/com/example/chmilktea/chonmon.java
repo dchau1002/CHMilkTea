7a13a2957ca435c1268f96374121c8a2dfa42b12
dchau1002 <dconghau1002@gmail.com>
2021-05-31 16:40:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chmilktea/chonmon.java
@@ -0,0 +1,14 @@
+package com.example.chmilktea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chonmon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chonmon);
+    }
+}
